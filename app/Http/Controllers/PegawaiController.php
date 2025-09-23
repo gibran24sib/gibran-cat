86f49f805808a5d84f7cf1e602a258dafedd57a0
gibran <gibran24si@mahasiswa.pcr.ac.id>
2025-09-23 10:35:39 +0700

    Latihan Passing Data dan Git

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class PegawaiController extends Controller
 {
-    //
+    public function index()
+    {
+        // Nama
+        $data['name'] = 'Gibran Ardelio';
+
+        // Hitung umur dari tanggal lahir (pakai DateTime biasa)
+        $birthdate = date_create('2006-04-25');
+        $today = date_create(date('Y-m-d'));
+        $age = date_diff($birthdate, $today)->y;
+        $data['my_age'] = $age;
+
+        // Hobi
+        $data['hobbies'] = ['Olahraga', 'Membaca', 'Ngoding', 'Lalok', 'Travelling'];
+
+        // Tanggal harus wisuda
+        $tglWisuda = '2028-09-05';
+        $data['tgl_harus_wisuda'] = $tglWisuda;
+
+        // Jarak hari ke wisuda
+        $tglWisudaTime = strtotime($tglWisuda);
+        $todayTime = strtotime(date('Y-m-d'));
+        $selisihHari = round(($tglWisudaTime - $todayTime) / (60 * 60 * 24));
+        $data['time_to_study_left'] = $selisihHari;
+
+        // Semester
+        $data['current_semester'] = 3;
+
+        // Info semester
+        if ($data['current_semester'] < 2) {
+            $data['semester_info'] = 'Masih Awal, Kejar TAK!';
+        } else {
+            $data['semester_info'] = 'Jangan main-main, kurang-kurangi main game!';
+        }
+
+        // Cita-cita
+        $data['future_goal'] = 'Menjadi Software Engineer';
+
+        return view('pegawai', $data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PegawaiController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,3 +25,5 @@ Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 Route::get('/about', function () {
     return view('halaman-about');
 });
+
+Route::get('/blade',[PegawaiController::class,'index']);
