e928f20448e31e075b456a8ab7b6dcd5e4855378
gibran24sib <gibran24si@mahasiswa.pcr.ac.id>
2025-09-30 10:20:45 +0700

    membuat form pertanyaan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\QuestionController;
 
 
 Route::get('/', function () {
@@ -27,3 +29,8 @@ Route::get('/about', function () {
 });
 
 Route::get('/blade',[PegawaiController::class,'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
+
+Route::get('/home',[HomeController::class,  'index']);
